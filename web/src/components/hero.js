6f7f876d73530d0da1f502002875af901a907305
Petros Patelis <petrospatelis@gmail.com>
2021-11-07 14:15:24 +0200

    adding hero

--- web/src/components/hero.js
+++ web/src/components/hero.js
@@ -11,40 +11,47 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Img from "gatsby-image";
 
-export default function Hero() {
+const Hero = () => {
   return (
   	<StaticQuery
       query={graphql`
-        query sanityHero {
-        	title
-        }
-      `}
+	      {
+	        sanityHero {
+	          title
+	          subtitle
+	          heroimage {
+	            asset {
+	              url
+	            }
+	          }
+	        }
+	      }
+	    `}
       render={data => (
-      	<Box sx={{ flexGrow: 1, width: '50%', margin: 'auto', fontWeight: 'bold', border: 1 }}>
-	    	<Card sx={{ maxWidth: '100%' }}>
-		      <CardMedia
-		        component="img"
-		        height="140"
-		        alt="green iguana"
-		      />
+      	<Box sx={{ flexGrow: 1, width: '50%', margin: 'auto', fontWeight: 'bold'}}>
+		    	<Card sx={{ maxWidth: '100%', boxShadow: 'none', borderRadius: 0, marginTop: 20 }}>
+			      <CardMedia
+			        component="img"
+			        height="100%"
+			        alt="green iguana"
+			        image={data.sanityHero.heroimage.asset.url}
+			      />
 
-		      <CardContent>
-		        <Typography gutterBottom variant="h5" component="div">
-		          {data.title}
-		        </Typography>
-		        <Typography variant="body2" color="text.secondary">
-		          Lizards are a widespread group of squamate reptiles, with over 6,000
-		          species, ranging across all continents except Antarctica
-		        </Typography>
-		      </CardContent>
-		      <CardActions>
-		        <Button size="small">Share</Button>
-		        <Button size="small">Learn More</Button>
-		      </CardActions>
-		    </Card>
-	    </Box>
+			      <CardContent style={{textAlign: 'center'}}>
+			        <Typography gutterBottom variant="h4" component="div">
+			          {data.sanityHero.title}
+			        </Typography>
+			        <Typography variant="h4" component="div" color="primary" fontWeight="bold">
+			          {data.sanityHero.subtitle}
+			        </Typography>
+			      </CardContent>
+			      
+			    </Card>
+		    </Box>
       )}
     />
     
   );
 };
+
+export default Hero;
